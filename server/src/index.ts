b35a3250f36e1d16b1bad0eeae411b728a6ed1c5
Jing Wei <jingweichan97@gmail.com>
2025-10-25 01:02:52 +0800

    fix: path

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,10 +13,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "../../client/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../../client/build", "index.html"));
 });
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
